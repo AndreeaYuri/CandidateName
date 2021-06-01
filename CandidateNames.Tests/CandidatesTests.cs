5dd09fd744d2000edc587be123e42fdb3ca0fce0
AndreeaYuri <andreea.horlescuu@gmail.com>
2021-06-01 18:27:43 +0100

    Add IsValidName + Tests

--- CandidateNames.Tests/CandidatesTests.cs
+++ CandidateNames.Tests/CandidatesTests.cs
@@ -42,5 +42,28 @@ namespace CandidateNames.Tests
             Assert.That(candidates.CombineAllApplicants(developers, testers).Count(p => p == "Gherca, Andreea"), Is.EqualTo(1));
         }
 
+        [Test]
+        [TestCase("Antonio, Mchaney")]
+        [TestCase("Mollie, Rabinowitz")]
+        public void IsValidName_PassValidName_ReturnTrue(string name)
+        {
+            var candidates = new Candidates();
+            bool result = candidates.IsaValidName(name);
+
+            Assert.That(true, Is.EqualTo(result));
+        }
+        [Test]
+        [TestCase("Rebecca")]
+        [TestCase("Long ,,Pigford")]
+        [TestCase("Marceline ,Polley")]
+        [TestCase("Gudrun&nbsp;&nbsp;  ,Caughman")]
+        public void IsValidName_PassInvalidName_ReturnFalse(string name)
+        {
+            var candidates = new Candidates();
+            bool result = candidates.IsaValidName(name);
+
+            Assert.That(false, Is.EqualTo(result));
+        }
+
     }
 }
--- CandidateNames/Candidate/Candidates.cs
+++ CandidateNames/Candidate/Candidates.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using CandidateNames.Sources;
 
 namespace CandidateNames.Candidate
@@ -18,5 +19,26 @@ namespace CandidateNames.Candidate
             }
             return allDevelopers.Union(allTesters).ToList();
         }
+        public bool IsaValidName(string name)
+        {
+            //the valid pattern for regex is : 
+            //^ asserts position at start of a line
+            //(..) group
+            //$ asserts position at the end of a line
+            //[a-zA-Z]  matches a single character in the range between a and z and between A and Z
+            //[a-zA - Z]+ at least one appearance of any alphabetic character
+            //[,] comma
+            //[\s] whitespace
+            //valid name: "Carrie, Kirker"
+            string pattern = @"^([a-zA-Z]+[,][\s][a-zA-Z]+)$";
+            Regex currencyRegex = new Regex(pattern);
+            if (currencyRegex.IsMatch(name))
+                return true;
+
+            return false;
+        }
+
+       
     }
+
 }
