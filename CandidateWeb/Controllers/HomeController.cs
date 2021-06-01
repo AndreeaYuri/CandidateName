0b845849fd411c630c15b75599dc0995c07b48e2
AndreeaYuri <andreea.horlescuu@gmail.com>
2021-06-01 18:55:01 +0100

    Add CandidateWeb project

---
+++ CandidateWeb/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using CandidateWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using CandidateNames.Candidate;
+
+namespace CandidateWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            var candidates = new Candidates();
+            var developers = candidates.GetDeveloperJobApplicants().ToList();
+            var testers = candidates.GetTesterJobApplicants().ToList();
+            var allApplicantsCombined = candidates.CombineAllApplicants(developers, testers);
+            var allCleanedApplicantsList = candidates.CleaningApplicantsList(allApplicantsCombined);
+            var lettersFrequency = candidates.LettersFrequency(allCleanedApplicantsList);
+            return View(new CandidateViewModel()
+            {
+                Developers = developers,
+                Testers = testers,
+                AllCandidates = allApplicantsCombined,
+                AllCleanedCandidates = allCleanedApplicantsList,
+                FrequencyLetters = lettersFrequency
+            });
+        }
+
+       
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CandidateWeb/Models/CandidateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidateWeb.Models
+{
+    public class CandidateViewModel
+    {
+
+        public List<string> Developers { get; set; }
+        public List<string> Testers { get; set; }
+        public List<string> AllCandidates { get; set; }
+        public List<string> AllCleanedCandidates { get; set; }
+        public Dictionary<string, int> FrequencyLetters { get; set; }
+    }
+}
